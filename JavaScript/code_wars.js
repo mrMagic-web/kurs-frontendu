438cc2982ba4726b8d43c03e8bc0bb7016effb10
mrMagic-web <maciejdrahusz@gmail.com>
2018-11-25 16:29:46 +0100

    Kurs js

---
+++ JavaScript/code_wars.js
@@ -0,0 +1,8 @@
+function persistence(num) {
+  let result = 0;
+  let arr = num.split("");
+
+  return arr;
+}
+
+console.log(persistance(44));
--- JavaScript/zadanie_warunek/script.js
+++ JavaScript/zadanie_warunek/script.js
@@ -1,5 +1,36 @@
-document.querySelector("#forma").addEventListener("submit", function(e) {
-  e.preventDefault();
+const kursDane = [
+  { name: "Kamil", age: 18, smart: true },
+  { name: "Kuba", age: 16, smart: true },
+  { name: "Filip", age: 18, smart: true },
+  { name: "Konrad", age: 18, smart: true },
+  { name: "Karolina", age: 18, smart: false }
+];
 
-  const wiek = document.querySelector("#wiek").value;
-});
+let text = "";
+let text2 = "";
+
+for (let i = 0; i < kursDane.length; i++) {
+  let wiek = kursDane[i].age > 17 ? " i jest dorosły" : " i nie może kupi piwa";
+  let jestSmart = kursDane[i].smart
+    ? " i jest super smart"
+    : " nie jest super smart";
+  text +=
+    "<p class='osoba'>" +
+    kursDane[i].name +
+    " ma " +
+    kursDane[i].age +
+    " lat " +
+    jestSmart +
+    wiek +
+    " </p> ";
+
+  text2 += `<p class='osoba'>${kursDane[i].name} ma ${
+    kursDane[i].age
+  } lat ${jestSmart} ${wiek} </p>`;
+}
+
+for (let osoba of kursDane) {
+  text += `<p>${osoba.age}</p>`;
+}
+
+document.querySelector(".text").innerHTML = text;
