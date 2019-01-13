a4ad58f3a572eeb77961552b51a34194fc16decb
mrMagic-web <maciejdrahusz@gmail.com>
2019-01-13 16:07:54 +0100

    SW example

--- JavaScript2/zadanie_roboty/script.js
+++ JavaScript2/zadanie_roboty/script.js
@@ -1,1 +1,21 @@
-console.log(roboty);
+let heroes;
+
+fetch('https://swapi.co/api/people/')
+    .then(res => res.json())
+    .then(response => heroes = response.results)
+
+function checkName() {
+    let name = document.getElementById('name').value;
+    console.log(heroes)
+    result = heroes.filter(hero => hero.name[0] === name)
+        .map(h => `<h3>${h.name}</h3>`)
+    document.querySelector('.hero').innerHTML = result;
+}
+
+
+// function displayHeros(response) {
+//     const heroes = response.results;
+//     result = heroes.filter(hero => hero.height > 170)
+//         .map(h => `<h3>${h.name}</h3>`)
+//     document.querySelector('.hero').innerHTML = result;
+// }
