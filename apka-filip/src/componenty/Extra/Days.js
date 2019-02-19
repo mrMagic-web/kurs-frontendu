3dbc21f26475fa0521f9c6cbded72270ccb7c4c1
mrMagic-web <maciejdrahusz@gmail.com>
2019-02-19 21:24:30 +0100

    Days poprawka

--- apka-filip/src/componenty/Extra/Days.js
+++ apka-filip/src/componenty/Extra/Days.js
@@ -2,12 +2,19 @@ import React from "react";
 
 const Days = props => {
 	const content = props.weather.list
-		? props.weather.list.slice(0, 3).map(w => (
-				<div className="day">
-					<h2>{Math.round(w.main.temp)}&#176;C</h2>
-					<img src={`http://openweathermap.org/img/w/${w.weather[0].icon}.png`} alt="" className="obr" />
-				</div>
-		  ))
+		? props.weather.list
+				.filter(e => {
+					const hour = new Date(e.dt_txt).getHours();
+					return hour === 15;
+				})
+				.slice(0, 3)
+				.map(w => (
+					<div className="day" key={w.dt}>
+						{console.log(w)}
+						<h2>{Math.round(w.main.temp)}&#176;C</h2>
+						<img src={`http://openweathermap.org/img/w/${w.weather[0].icon}.png`} alt="" className="obr" />
+					</div>
+				))
 		: "";
 
 	return content;
